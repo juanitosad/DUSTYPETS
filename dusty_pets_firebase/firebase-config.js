13cd717d9c6a7b9f12330e3af51522e2cd724267
juanitosad <juangaelcorralesramos@gmail.com>
2025-07-14 21:27:15 -0600

    Add files via upload

---
+++ dusty_pets_firebase/firebase-config.js
@@ -0,0 +1,13 @@
+// Rellena esto con tu configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBWqP0ERkmH_BpkJ6kiXOMgs3N3RTnxf4Y",
+  authDomain: "dus1-5186f.firebaseapp.com",
+  projectId: "dus1-5186f",
+  storageBucket: "dus1-5186f.firebasestorage.app",
+  messagingSenderId: "375490850162",
+  appId: "1:375490850162:web:27f3c86056d9baf63801eb",
+  measurementId: "G-XSECE262H8"
+};
+firebase.initializeApp(firebaseConfig);
+
+
